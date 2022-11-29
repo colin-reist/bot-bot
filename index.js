c5ae9ead9731d7d0d1e3a356cc66112d668b35ff
Colin Reist <colinreist04@gmail.com>
2022-11-30 00:31:36 +0100

    Ajout de la gestion de commande sur Paradise

--- index.js
+++ index.js
@@ -1,63 +1,39 @@
-// Require the necessary discord.js classes
+/* eslint-disable brace-style */
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.command = new Collection();
-
-// Help to construct a path to the commands directory
+client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
-// Return an array of all the file name with ".js" in the fodlder "commands"
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	}
-	else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+	client.commands.set(command.data.name, command);
 }
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.once(Events.ClientReady, () => {
+	console.log('Ready!');
 });
 
-// Log in to Discord with your client's token
-client.login(token);
-
-// Si le "/" est utilisé par l'utilisateur on retourne dans la console les commandes disponibles pour le robot
-client.on(Events.InteractionCreate, interaction => {
-	console.log(interaction);
-});
-
-// Affiche seulement les commandes qui corresponds à ce qu'il y a après le "/"
-// Si rien ne correspond aucune commande est retourné dans la console
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+	const command = client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+	if (!command) return;
 
 	try {
 		await command.execute(interaction);
-	}
-	catch (error) {
+	} catch (error) {
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
+
+client.login(token);
 
