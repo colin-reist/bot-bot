69751bd905d096df7db695e31d203863d32992c3
reiscol <colinreist04@gmail.com>
2022-11-30 15:15:35 +0100

    réparationd de quelque erreur dans les embeds mais des erreurs sont toujours là

---
+++ commands/embed.js
@@ -0,0 +1,32 @@
+// at the top of your file
+const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('embed')
+		.setDescription('return an embed'),
+	async execute(interaction, client) {
+		// inside a command, event listener, etc.
+		const exampleEmbed = new EmbedBuilder()
+			.setColor(0x0099FF)
+			.setTitle('Some title')
+			.setURL('https://discord.js.org/')
+			.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+			.setDescription('Some description here')
+			.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+			.addFields(
+				{ name: 'Regular field title', value: 'Some value here' },
+				{ name: '\u200B', value: '\u200B' },
+				{ name: 'Inline field title', value: 'Some value here', inline: true },
+				{ name: 'Inline field title', value: 'Some value here', inline: true },
+			)
+			.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+			.setImage('https://i.imgur.com/AfFp7pu.png')
+			.setTimestamp()
+			.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+		await interaction.reply({
+			embeds: [exampleEmbed],
+		});
+	},
+};
